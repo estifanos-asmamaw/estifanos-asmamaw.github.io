cf7b8fac4e6f97290565fdd45251f93e4bfc3039
kenenisa alemayheu <kenenisaalemayhu0@gmail.com>
2023-10-02 17:19:44 +0300

    updated build

--- build.js
+++ build.js
@@ -3,5 +3,5 @@ const html = `
     ${fs.readFileSync('data.js').toString()};
     window.data = data;`
 const index = fs.readFileSync('200.html').toString()
-
+console.log({index})
 fs.writeFileSync('200.html', index.replace('window.data = data',html))
