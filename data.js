6900dad426d4fd38d89ab3a6a9349a4f68d060d4
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2023-10-02 14:35:12 +0300

    init

---
+++ data.js
@@ -0,0 +1,293 @@
+var data = {
+    gallery: [
+        {
+            id: 38,
+            title: "ጨረቃ",
+            body: "its been a while sice i worked on typography so i was trying out new amharic fonts and thats how i found this amazing piece called ጨረቃ , and who knows this might end up being a font so if we ever decied to make it happen ill sure share the process so stay tuned and i have made two versions of it in case it looked better in the other one.",
+            tag: "ethiopia, fantasy, photoshop, ጨረቃ, night, black, typography, font "
+        },
+        {
+            id: 37,
+            title: "ጨረቃ in Black",
+            body: "its been a while sice i worked on typography so i was trying out new amharic fonts and thats how i found this amazing piece called ጨረቃ , and who knows this might end up being a font so if we ever decied to make it happen ill sure share the process so stay tuned.",
+            tag: "ethiopia, fantasy, photoshop, ጨረቃ, night, black, moody, typography, font"
+        },
+        {
+            id: 36,
+            title: "kelቅል NFT",
+            body: "kelቅል is one of our very own first NFT collection and it is based on the first kelቅል segment which show the mix up of our ethiopians traditional and other cultures.",
+            tag: "ethiopia, fantasy, photoshop, Habeshan, NFT, traditional, kelቅል, design, cultural "
+        },
+
+        {
+            id: 35,
+            title: "HABESHAN LOGIC 3",
+            body: "The third part for HL bring out a nice and diffrent look than the other parts and it conntribute to the traditional way of creating our spices.",
+            tag: "ethiopia, fantasy, photoshop, Habeshan, logic, traditional, spices, design "
+        },
+
+        {
+            id: 34,
+            title: "SKY WALKER 3",
+            body: "this is the third part for the sky walker segment As You Saw In The Previous Parts We Like To Change the Scene In Every Part.",
+            tag: "ethiopia, fantasy, photoshop, inspired, earth, Moon, warm, dreamer, space , Stars  "
+        },
+        
+        {
+            id: 33,
+            title: "THE Dreamer",
+            body: "this is a design for all those people out thier struggling on that creative voage ..... KEEP UP.",
+            tag: "ethiopia, fantasy, photoshop, inspired, highlights, wonder, warm, dreamer, night  "
+        },
+        {
+            id: 32,
+            title: "የ ህልም ጉዞ",
+            body: "this is a funny design that has a fantasized dreamy look and its a mouthfull got warm colors with iconic ethiopian bus",
+            tag: "ethiopia, fantasy, photoshop, inspired, anbessabus, wonder, warm, scratch, bus, clouds "
+        },
+        {
+            id: 31,
+            title: "HOME",
+            body: "here is my number one favorite design of all time that is half photo half illustration and it is titled home",
+            tag: "ethiopia, fantasy, photoshop, wonder, pexles, scratch, home, illustration"
+        },
+        {
+            id: 30,
+            title: "Thin Wall and Tall Cellings",
+            body: "this is a music inspired design with the same title and just made it from scratch ",
+            tag: "ethiopia, fantasy, photoshop, inspired, musicinspired, wonder, pexles, scratch "
+        },
+        {
+            id: 29,
+            title: "SKY WALKER 2",
+            body: "sky walker is back with another one got a little spin on it from the first part the enviroment is changed ",
+            tag: "ethiopia, illustration, photoshop, fantasy, creativebubble, wonder, shiloutte, part2 "
+        },
+        {
+            id: 28,
+            title: "SKY WALKER",
+            body: "sky walker is a brand for a t shirt its done on as shiloutte illustration its about being in that creative bubble being lost in the process ",
+            tag: "ethiopia, illustration, photoshop, fantasy, creativebubble, wonder, shiloutte, part1 "
+        },
+        {
+            id: 27,
+            title: "NIQ Fantasy diaries Ep 3",
+            body: "the third part of niq fantasy diaries is here and i like how it turned out .... enjoy cold coffee ",
+            tag: "ethiopia, photoshop, NIQ, fantasy, segment, wonder, coffeepot, huge, coffee, coldcoffee, ep3 "
+        },
+        {
+            id: 26,
+            title: "NIQ Fantasy diaries Ep 1",
+            body: "here is a new segment where i show my fantasys just wonder u know designs of imagination and other stuff ",
+            tag: "ethiopia, photoshop, NIQ, fantasy, segment, battry, roof, wonder, wall, huge, ep1 "
+        },
+        {
+            id: 25,
+            title: "NIQ Fantasy diaries Ep 2",
+            body: "this is the second part of the segment and kinda cool .. just saw the cracks of the house and BOOM. ",
+            tag: "ethiopia, photoshop, NIQ, fantasy, segment, stars, modern, wonder, wall, ep2 "
+        },
+        {
+            id: 24,
+            title: "kelቅል",
+            body: "kelቅል is a new segment i started its about clasing two cultures its a colab ENJOY.",
+            tag: "ethiopia, photoshop, kelቅል, starwars, segment, ጋቢ, modern, habeshan"
+        },
+        {
+            id: 23,
+            title: "kassmasse",
+            body: "kassmasse is one of the new uprising artist's and im big fan so i made him a poster",
+            tag: "ethiopia, photoshop, kassmasse, artist, new, poster, antasr"
+        },
+        {
+            id: 22,
+            title: "UNIQ design poster 2",
+            body: "just made a new cool poster for my channel and its kinda silent and more eligant than the first one",
+            tag: "ethiopia, photoshop, eligant, silent, channelgoodness, poster, purple"
+        },
+        {
+            id: 21,
+            title: "UNIQ design poster 1",
+            body: "just made a new cool poster for my channel and its glitch and skew combo",
+            tag: "ethiopia, photoshop, posteruse, glitter, channelgoodness, poster, glitch"
+        },
+        {
+            id: 20,
+            title: "prom flyer",
+            body: "this a template for a flyer i did for a client",
+            tag: "ethiopia, photoshop, poster, glitter, prom, forclinet, flyer"
+        },
+        {
+            id: 19,
+            title: "2014",
+            body: "2014 is HERE ... its about to be a great year i can feel it  anyway have a great holiday",
+            tag: "ethiopia, photoshop, poster, holidayposter, holiday, 2014, መስከረም1"
+        },
+        {
+            id: 18,
+            title: "ልደታ",
+            body: "one of the best holidays of all time is here",
+            tag: "ethiopia, photoshop, poster, holidayposter, holiday, ግንቦት1, ልደታ"
+        },
+        {
+            id: 17,
+            title: "ቀለም",
+            body: "here is a poster i did for a party for a client",
+            tag: "ethiopia, photoshop, kelem, party, flyer, poster, ቀለም"
+        },
+        {
+            id: 16,
+            title: "HABESHAN LOGIC",
+            body: "habeshan logic is a design for a t shirt brand and this is the developed version for the t shirt back",
+            tag: "ethiopia, photoshop, firstrelase, brand, tshirt, HB, habeshanlogic"
+        },
+        {
+            id: 15,
+            title: "HABESHAN LOGIC",
+            body: "habeshan logic is a design for a t shirt brand and here this one is the first relase",
+            tag: "ethiopia, photoshop, firstrelase, brand, tshirt, HB, habeshanlogic"
+        },
+        {
+            id: 14,
+            title: "HABESHAN LOGIC",
+            body: "habeshan logic is a design for a t shirt brand and here this one is the first relase",
+            tag: "ethiopia, photoshop, firstrelase, brand, tshirt, HB, habeshanlogic"
+        },
+        {
+            id: 13,
+            title: "ጳጉሜ 1",
+            body: "ጳጉሜ 1 የኢትዮጵያዊነት ቀን",
+            tag: "ethiopia, photoshop, conceptual, telegram, pagume1, one, ethiopian'sday"
+        },
+        {
+            id: 12,
+            title: "savage streets",
+            body: " another text effect i did for a telegram channel ",
+            tag: "ethiopia, photoshop, typography, telegram, texteffect, tg, savagestreets"
+        },
+        {
+            id: 11,
+            title: "አመለወርቅ",
+            body: " a funny try out typography that might turn out to be a font one day who knows a verse frome kassmasse's song",
+            tag: "ethiopia, photoshop, typography, track, font, kassmasse,  amelework, texteffect"
+        },
+        {
+            id: 10,
+            title: "MUSIC COVER ART for JOSIAH",
+            body: " artist josiah come back with the remix for his latest ep and i made him this cool coverart for him",
+            tag: "ethiopia, photoshop, josiah, track, coverart, newrealased, artist"
+        },
+        {
+            id: 9,
+            title: "MUSIC COVER ART for dagi dane",
+            body: " artist dagi dane just relased a new track and got the chance to do his cover art",
+            tag: "ethiopia, photoshop, dagidane, track, coverart, newrealased, artist"
+        },
+        {
+            id: 8,
+            title: "አበበ በቂላ",
+            body: " one of our heroes አበበ በቂላ is back on the new segment kelቅል.",
+            tag: "ethiopia, photoshop, abebebekela, kelቅል, አበበበቂላ, typography"
+        },
+        {
+            id: 7,
+            title: "Hagere",
+            body: " hagere is a reality based design made to prvoke the feeling that our countrys pin points as a milestone.",
+            tag: "ethiopia, photoshop, milestone, pages, wrap"
+        },  
+        {
+            id: 6,
+            title: "Crow",
+            body: " this is a gaming cover art i did for a friend title crow after the player and its build up from some pics i found.",
+            tag: "crow, photoshop, gaming, COD, action, highlights"
+        },
+        {
+            id: 5,
+            title: "Priest",
+            body: " i found this pic and i changed up some things add some highlights u here and there but it really turned out fine.",
+            tag: "photoshop, priest, B&W, blackandwhite, highlights, priestofethiopia"
+        },
+        {
+            id: 4,
+            title: "Ethians Armada",
+            body: " here is something to look up mashup of ethiopians armada and the iconic AT AT from star wars .",
+            tag: "army, starwars, kelቅል, photoshop, old, blackandwhite, ATAT, ethians, ethiopianarmy"
+        },
+        {
+            id: 3,
+            title: "Tesfa",
+            body: " this is an old piece of mine i developed it based on the fact the hope we put first at some point in our life.",
+            tag: "flower, hope, photoshop, rose, sparkel"
+        },
+        {
+            id: 2,
+            title: "class",
+            body: " here is some bannger black and white picture with some color retouch ..... we are with class since then.",
+            tag: "B&W, retouch, class, photoshop , blackandwhite"
+        },
+        {
+            id: 1,
+            title: "ADMAS",
+            body: " here is some try out that turned out to be a fine art .......i think.",
+            tag: "illustration, admas, sunset, sun, conceptual"
+        },
+           
+    ],
+    blog: [
+        {
+            id: 1,
+            title: "Milestone",
+            body: "Hello Everyone Estif(NIQ) Here And First Of All I Wanna Start Up By Sayin How Is The Website? And Second Wow I Never Thought I Would Go This Far I Mean I love Graphics Designing But It Happned So Fast One Day i Was Opening Photoshop For The First Time But Now I Have A Channel And A Website Wow And The Thing I Wanna Say Is That Continuing To Do Things Is Great What Ever You're Field Is And Lastly I Wanna Give Thanks For All The People Who Helped Me To Be In This Place And All The Fans Who Enjoyed My Work And ENJOY The Launch Of My Website.",
+            tag: "Milestone, thanks, NIQ, thought"
+        },
+       
+    ],
+    projects: [
+        {
+            id: 1,
+            title: "SKY WALKER",
+            belongs: "personal",
+            body: "SKY WALKER Is An Orignal Brand Of Ours Soon To Be.",
+            start: "20 oct 2019",
+            end: "ongoing"
+        },
+      
+    ],
+    skills: [
+        {
+            id: 1,
+            name: 'Photoshop',
+            amount: 100
+        },
+        {
+            id: 2,
+            name: 'Typography',
+            amount: 90
+        },
+        {
+            id: 3,
+            name: 'Illustration',
+            amount: 90
+        },
+        {
+            id: 4,
+            name: 'Coding',
+            amount: 40
+        },
+        {
+            id: 5,
+            name: 'cinimatography',
+            amount: 70
+        },
+        
+    ],
+    reviews: [
+        {
+            id: 1,
+            name: 'We Decor',
+            type: 'Flayer Template & Poster',
+            text: 'We Gave UNIQ Design To Design Our Flayer Template, And By Their Outstanding Result And Incredible Speed, We Decided To Work More With Them And Gave A Project To Design Out Our Main Poster For Our Upcoming Event, Once Again The Poster Was Designed With An Amazing And Very Creative Way And For That We Would Like To Appreciate Thier Dedication And Respect For Clients. And We Hope To Work More With Them In The Future <br> We Decor <br>.',
+            rate: 4.7
+        },
+    ]
+}
